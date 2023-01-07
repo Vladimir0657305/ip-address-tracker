4dd08fd9079d33eff48d378a89ad75ee89467de0
Vladimir0657305 <vv0657305@gmail.com>
2023-01-07 18:54:16 +0200

    rebuild

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -1,4 +1,4 @@
-// https://sys.airtel.lv/ip2country/193.194.110.111/?full=true
+// https://sys.airtel.lv/ip2country/157.230.229.134/?full=true
 // https://github.com/public-apis/public-apis#geocoding
 // https://www.frontendmentor.io/challenges/ip-address-tracker-I8-0yYAH0/hub
 // https://www.freecodecamp.org/news/how-to-set-up-a-custom-mapbox-basemap-with-gatsby-and-react-leaflet/
